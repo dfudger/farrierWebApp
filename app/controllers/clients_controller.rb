c1854d82251e565e1b0924f0ae4fedc74a63908a
dfudger <daniellefudger@gmail.com>
2015-08-28 22:26:45 -0400

    Added database

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,28 @@
 class ClientsController < ApplicationController
+	def index
+		@clients = Client.all
+	end
+
+	def show
+		@client = Client.find(params[:id])
+	end
+
 	def new
+		@client = Client.new
+	end
+
+	def create
+		@client = Client.new(client_params)
+
+		if @client.save
+			redirect_to @client
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def client_params
+		params.require(:client).permit(:name, :address, :email, :phone, :comment)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150827213841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
