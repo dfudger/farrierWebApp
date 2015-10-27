eaf9f695b62cf5437f8211ce1e516abbb29ad9dc
dfudger <daniellefudger@gmail.com>
2015-10-26 21:00:00 -0400

    Able to add horses to clients profile. Saved to client. Can add new horses from client page.

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,2 +1,12 @@
 class HorsesController < ApplicationController
+  def create
+    @client = Client.find(params[:client_id])
+    @horse = @client.horses.create(horse_params)
+    redirect_to client_path(@client)
+  end
+
+  private
+  def horse_params
+    params.require(:horse).permit(:name, :comment)
+  end
 end
