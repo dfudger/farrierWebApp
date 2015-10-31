e8abc220ed27ad10517df5abc47c98bdf8c6219d
dfudger <daniellefudger@gmail.com>
2015-10-31 16:30:33 -0400

    Cleaned up the horse model for viewing horses

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,10 +1,20 @@
 class HorsesController < ApplicationController
   http_basic_authenticate_with name: "dani", password: "secret", only: :destroy
-  
+
   def create
     @client = Client.find(params[:client_id])
     @horse = @client.horses.create(horse_params)
     redirect_to client_path(@client)
+  end
+
+  def new
+    @client = Client.find(params[:client_id])
+		@horse = Horse.new
+	end
+
+  def show
+    @client = Client.find(params[:client_id])
+    @horse = Horse.find(params[:id])
   end
 
   def destroy
