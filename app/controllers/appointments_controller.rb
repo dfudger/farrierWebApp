e12750d1cba563a5f82ee2f7d6a36193c8f301f2
dfudger <daniellefudger@gmail.com>
2015-11-11 20:32:39 -0500

    Able to edit appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -7,6 +7,29 @@ class AppointmentsController < ApplicationController
     redirect_to client_horse_path(@client, @horse)
   end
 
+  def new
+    @horse = Horse.find(params[:horse_id])
+    @appointment = Appointment.new
+	end
+
+  def edit
+    @horse = Horse.find(params[:horse_id])
+    @client = @horse.client
+    @appointment = @horse.appointments.create(appointment_params)
+  end
+
+  def update
+    @horse = Horse.find(params[:horse_id])
+    @client = @horse.client
+    @appointment = @horse.appointments.create(appointment_params)
+
+    if @appointment.update(appointment_params)
+		  redirect_to client_horse_path(@client, @horse)
+		else
+		  render 'edit'
+		end
+	end
+
   private
   def appointment_params
     params.require(:appointment).permit(:start, :end, :location)
