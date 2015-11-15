d43f3d0e54bb30a4c003c1b5dc3204913341e703
dfudger <daniellefudger@gmail.com>
2015-11-15 15:48:26 -0500

    Forgot to add stable routes

--- app/controllers/stables_controller.rb
+++ app/controllers/stables_controller.rb
@@ -1,8 +1,20 @@
 class StablesController < ApplicationController
   def new
+    @stable = Stable.new
   end
 
   def create
+    @stable = Stable.new(stable_params)
+
+    if @stable.save
+      redirect_to @stable
+    else
+      render 'new'
+    end
   end
-  
+
+  private
+  def stable_params
+		params.require(:stable).permit(:name, :owner, :phoneNo, :address, :email, :comment)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :horses do
     resources :appointments
   end
-
+  resources :stables
   get 'horses' => 'horses#index'
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
