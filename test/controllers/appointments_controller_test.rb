2602e67431cef109b515442ef1461f5def5f291d
dfudger <daniellefudger@gmail.com>
2016-01-12 21:55:29 -0500

    Minor update to Horses page, commented out appointments until implemented

--- test/controllers/appointments_controller_test.rb
+++ test/controllers/appointments_controller_test.rb
@@ -5,45 +5,45 @@ class AppointmentsControllerTest < ActionController::TestCase
     @appointment = appointments(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:appointments)
-  end
+  #test "should get index" do
+  #  get :index
+  #  assert_response :success
+  #  assert_not_nil assigns(:appointments)
+  #end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  #test "should get new" do
+  #  get :new
+  #  assert_response :success
+  #end
 
-  test "should create appointment" do
-    assert_difference('Appointment.count') do
-      post :create, appointment: { comment: @appointment.comment, end: @appointment.end, location: @appointment.location, start: @appointment.start }
-    end
+  #test "should create appointment" do
+  #  assert_difference('Appointment.count') do
+  #    post :create, appointment: { comment: @appointment.comment, end: @appointment.end, location: @appointment.location, start: @appointment.start }
+  #  end
 
-    assert_redirected_to appointment_path(assigns(:appointment))
-  end
+  #  assert_redirected_to appointment_path(assigns(:appointment))
+  #end
 
-  test "should show appointment" do
-    get :show, id: @appointment
-    assert_response :success
-  end
+  #test "should show appointment" do
+  #  get :show, id: @appointment
+  #  assert_response :success
+  #end
 
-  test "should get edit" do
-    get :edit, id: @appointment
-    assert_response :success
-  end
+  #test "should get edit" do
+  #  get :edit, id: @appointment
+  #  assert_response :success
+  #end
 
-  test "should update appointment" do
-    patch :update, id: @appointment, appointment: { comment: @appointment.comment, end: @appointment.end, location: @appointment.location, start: @appointment.start }
-    assert_redirected_to appointment_path(assigns(:appointment))
-  end
+  #test "should update appointment" do
+  #  patch :update, id: @appointment, appointment: { comment: @appointment.comment, end: @appointment.end, location: @appointment.location, start: @appointment.start }
+  #  assert_redirected_to appointment_path(assigns(:appointment))
+  #end
 
-  test "should destroy appointment" do
-    assert_difference('Appointment.count', -1) do
-      delete :destroy, id: @appointment
-    end
+  #test "should destroy appointment" do
+  #  assert_difference('Appointment.count', -1) do
+  #    delete :destroy, id: @appointment
+  #  end
 
-    assert_redirected_to appointments_path
-  end
+  #  assert_redirected_to appointments_path
+  #end
 end
